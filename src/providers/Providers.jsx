4090668c1a7ae5ab387e8325ac5f30025ab3b07d
jihadsoyon <jihadsoyon@gmail.com>
2026-07-28 01:06:15 +0600

    Setup Tailwind CSS, HeroUI provider and base layout

---
+++ src/providers/Providers.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { HeroUIProvider } from "@heroui/react";
+import { Toaster } from "react-hot-toast";
+
+export default function Providers({ children }) {
+  return (
+    <HeroUIProvider>
+      {children}
+      <Toaster position="top-center" reverseOrder={false} />
+    </HeroUIProvider>
+  );
+}
